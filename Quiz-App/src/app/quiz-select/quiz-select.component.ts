7838343df6f78593e004deaf6e62d95c2b221ea7
Eleonora <eleonoracasto93@gmail.com>
2023-06-21 17:12:19 +0200

    c

--- Quiz-App/src/app/quiz-select/quiz-select.component.ts
+++ Quiz-App/src/app/quiz-select/quiz-select.component.ts
@@ -9,33 +9,38 @@ import { HttpClient } from '@angular/common/http';
 })
 export class QuizSelectComponent {
   apiData: any;
+  categories: any[] = []; // Array to store the categories
+  difficulties: any[] = []; // Array to store the categories
+
   constructor(private http: HttpClient) {}
 
-  getCategory(): void {
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories(): void {
     const urlCategories = `https://opentdb.com/api_category.php`;
     this.http.get<any>(urlCategories).subscribe(
       (data) => {
         this.apiData = data; // Assegna i dati ottenuti dall'API alla variabile apiData
+        this.categories = this.apiData.trivia_categories; // Assign categories to the array
       },
       (error) => {
         console.error('Failed to get data:', error);
       }
     );
-    /* console.log(this.apiData); */
-    console.log(this.apiData.trivia_categories[0].name);
-    let categories = this.apiData.trivia_categories[0].name;
-  }
-}
-/* getData() {
-    this.dataApi.getData().subscribe((data) => {
-      console.log(data);
-      this.apiData = data;
-      console.log(this.apiData.results[1].category);
-    });
   }
 
-  /*   get getCategory(): HttpClient {
-    return this.contactForm.get('details')?.get('languageSkills') as FormArray; // contenuto lo identifico come FormArray
+  getDifficulties(): void {
+    const urlCategories = `https://opentdb.com/api_difficulty.php`;
+    this.http.get<any>(urlCategories).subscribe(
+      (data) => {
+        this.apiData = data; // Assegna i dati ottenuti dall'API alla variabile apiData
+        this.difficulties = this.apiData.trivia_difficulties; // Assign categories to the array
+      },
+      (error) => {
+        console.error('Failed to get data:', error);
+      }
+    );
   }
 }
- */
